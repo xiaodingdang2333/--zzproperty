678a433beb90ea48ff79647db2c7b1ba6c508127
zhangzheng <zzzhangzheng@rd.netease.com>
2019-09-27 17:16:19 +0800

    1

--- 全排列.cpp
+++ 全排列.cpp
@@ -1,25 +1,24 @@
 #include <string>
 #include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
 using namespace std;
+void permutation(vector<string>&res,string str,int index,int len)//string str 加&就不按字典序输出，不加就按字典序输出 
+{
+	if(index==len)res.push_back(str);
+	for(int i=index;i<len;i++)
+	{
+		if(i!=index&&str[i]==str[index])continue;
+		swap(str[i],str[index]);
+		permutation(res,str,index+1,len);
+	}
+ } 
 int main()
 {
-	//输入不定长字符串到string
-
-	string a = "";
-	char j=getchar();
-	do
-	{
-		a.push_back(j);
-	} while ((j = getchar() != '\n');
-	cout << a << endl;
-	//每隔一个输入到string
-	string b = "";
-	char k;
-	do
-	{
-		cin >> k;
-		a.push_back(k);
-	} while (getchar() != '\n');
-	cout << b;
+	string str="1233";
+	vector<string>res;
+	permutation(res,str,0,str.length());
+	for(int i=0;i<res.size();i++)cout<<res[i]<<endl;
 	system("pause");
 }
